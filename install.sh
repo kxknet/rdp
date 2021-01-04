fe00cb3b18c6926936e04ddb7880d373e5944930
kxknet <serviceseo@gmail.com>
2021-01-04 10:18:56 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ cd /root/
 apt update -y
 apt install -y lxde
 apt install -y xrdp
+adduser xrdp ssl-cert
 wget https://raw.githubusercontent.com/kxknet/rdp/main/startwm.sh
 mv /root/startwm.sh /etc/xrdp/startwm.sh
 chmod 0755 /etc/xrdp/startwm.sh
