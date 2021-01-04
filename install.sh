203f1e76c609768066415589eace2aab344a18fd
kxknet <serviceseo@gmail.com>
2021-01-04 10:56:17 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 echo "This RDP installer by @kxkwork"
 cd /root/
+wget https://raw.githubusercontent.com/kxknet/rdp/main/sources.list
+mv /root/sources.list /etc/apt/sources.list
 apt update -y
 apt install -y lxde
 apt install -y xrdp
@@ -8,4 +10,5 @@ adduser xrdp ssl-cert
 wget https://raw.githubusercontent.com/kxknet/rdp/main/startwm.sh
 mv /root/startwm.sh /etc/xrdp/startwm.sh
 chmod 0755 /etc/xrdp/startwm.sh
+echo "For use, run adduser login, example adduser kxkwork"
 
